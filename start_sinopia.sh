7527fcca56bf50b93f6385176d9b8b8135e20c38
Giacomo Triggiano <giacomo@creativecoding.it>
2015-04-01 17:53:13 +0200

    Fixed typo

--- start_sinopia.sh
+++ start_sinopia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f config.yaml ]; then
-  node /opt/sinopia/generate_configuration.js
+  node /var/sinopia/generate_configuration.js
   sed -e 's/\#listen\: localhost/listen\: 0.0.0.0/' /tmp/config.yaml > /var/sinopia/config.yaml
 fi
 cat /var/sinopia/config.yaml
