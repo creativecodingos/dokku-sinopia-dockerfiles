29a2a8dfdb4c1c6adeca98b96b5ab5d3084f4f2b
Giacomo Triggiano <giacomo@creativecoding.it>
2015-04-01 20:14:17 +0200

    Delete start_sinopia.sh

--- start_sinopia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f config.yaml ]; then
-  node /var/sinopia/generate_configuration.js
-  sed -e 's/\#listen\: localhost/listen\: 0.0.0.0/' /tmp/config.yaml > /var/sinopia/config.yaml
-fi
-cat /var/sinopia/config.yaml
-node /var/sinopia/node_modules/sinopia/bin/sinopia --config /var/sinopia/config.yaml
